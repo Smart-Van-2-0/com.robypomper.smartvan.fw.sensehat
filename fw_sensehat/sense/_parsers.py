620fc5d281c67bc2281631c095464a5c60ffbb72
Roberto Pompermaier <robypomper@gmail.com>
2024-08-05 17:27:58 +0200

    refactor(Props): Removed unnecessary parser

--- fw_sensehat/sense/_parsers.py
+++ fw_sensehat/sense/_parsers.py
@@ -1,18 +1,6 @@
 #!/usr/bin/python3
 
 
-def props_parser_vin(raw_value: str) -> bool:
-    try:
-        if raw_value.upper() == "GOOD":
-            return True
-        elif raw_value.upper() == "NG":
-            return False
-        else:
-            raise ValueError("Can't cast '{}' into {}, invalid value".format(raw_value, "float"))
-    except Exception:
-        raise ValueError("Can't cast '{}' into {}".format(raw_value, "float"))
-
-
 def calc_temperature(property_cache) -> float:
     try:
         return property_cache['temperature_2']['value']
