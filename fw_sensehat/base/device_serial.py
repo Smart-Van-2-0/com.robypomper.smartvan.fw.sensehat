79701cf77ad8f4537cb9128fdaa983964a038ce9
Roberto Pompermaier <robypomper@gmail.com>
2024-09-06 17:15:20 +0200

    refactor(All): Moved firmware base file into base dir

--- fw_sensehat/base/device_serial.py
+++ fw_sensehat/base/device_serial.py
@@ -3,8 +3,8 @@
 import os
 import serial
 
-from fw_sensehat.device import DeviceAbs
-from fw_sensehat.commons import dev_type_to_code
+from fw_sensehat.base.device import DeviceAbs
+from fw_sensehat.base.commons import dev_type_to_code
 
 
 class DeviceSerial(DeviceAbs):
